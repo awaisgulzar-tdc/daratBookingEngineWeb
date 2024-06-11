71ab700c8b8f19ed8c1681f76c34d8c717be31de
awaisgulzar-tdc <awais.gulzar@thedevcorporate.com>
2024-06-11 12:45:21 +0500

    adminpanel page.js

--- src/app/adminpanel/page.js
+++ src/app/adminpanel/page.js
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import React, { useEffect, useState } from "react";
 import Box from "@mui/material/Box";
 import AdminPannel from "./_components/AdminPannel";
@@ -8,9 +8,14 @@ function Page() {
   const [loading, setLoading] = useState(true);
   const router = useRouter();
 
+  // Change 1: Conditional return to prevent server-side rendering issues
+  if (typeof window === 'undefined') {
+    return null;
+  }
+
   useEffect(() => {
-    // Check if running in the browser environment
-    if (typeof window !== "undefined") {
+    // Change 2: Ensure this runs only on the client side
+    if (typeof window !== 'undefined') {
       const token = localStorage.getItem("token");
       const role = localStorage.getItem("role");
 
